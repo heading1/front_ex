c16b4c309db220a92ff53cc4762d6b144e5d5596
Circlewee <pnj1118@naver.com>
2022-06-08 17:34:11 +0900

    render() 모듈화

--- src/pnj/counter/CounterApp.js
+++ src/pnj/counter/CounterApp.js
@@ -1,14 +1,14 @@
 import useState from '../libraries/useState.js';
 
 export default function CounterApp() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
   window.increment = () => setCount(count + 1);
 
   return `
     <div>
       <h1>Hello!</h1>
-      <strong>count: ${count} </strong>
+      <strong>count: ${count}  </strong>
       <button onclick="increment()">증가</button>
     </div>
   `;
