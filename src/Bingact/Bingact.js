dffe2ed62d0e53281d654929e99b70553144af57
Circlewee <pnj1118@naver.com>
2022-06-12 18:51:44 +0900

    경로 변경 및 재조정 전까지

---
+++ src/Bingact/Bingact.js
@@ -0,0 +1,171 @@
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function (cb) {
+    var start = Date.now();
+    return setTimeout(function () {
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        },
+      });
+    }, 1);
+  };
+
+window.cancelIdleCallback =
+  window.cancelIdleCallback ||
+  function (id) {
+    clearTimeout(id);
+  };
+
+/**
+ * define new Bingact element
+ * @param {string} type
+ * @param {object} props
+ * @param  {...Bingact.element} children
+ * @returns Bingact Element
+ */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createDom(fiber) {
+  const dom =
+    fiber.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(fiber.type);
+
+  const isProperty = (key) => key !== 'children';
+
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = fiber.props[name];
+    });
+
+  return dom;
+}
+
+function commitRoot() {
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+// 다음 작업 단위를 설정
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    alternate: currentRoot,
+  };
+  nextUnitOfWork = wipRoot;
+}
+
+// 수행해야할 작업
+let nextUnitOfWork = null;
+// 오래된 Root, 비교를 위한 Root이다.
+let currentRoot = null;
+// 구성중인 Root, 리렌더링을 위한 Root이다.
+let wipRoot = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1; // 작업을 수행하기 위한 충분한 시간이 존재한다면 false, 아니면 true
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot(wipRoot);
+  }
+
+  window.requestIdleCallback(workLoop); // 작업을 수행하고나면 다시 IdleCallback을 등록한다.
+}
+
+// 초기 IdleCallback 등록, 렌더링의 시작
+window.requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  // 모든 children을 새로운 fiber로 생성하는 반복문
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
+
+const useState = () => {};
+
+const Bingact = {
+  createElement,
+  render,
+  useState,
+};
+
+export default Bingact;
